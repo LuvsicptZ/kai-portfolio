cbca3eea55bb2e1793efc53a929690f146d188f2
LuvsicptZ <Ryan197427@outlook.com>
2025-12-12 01:23:31 +1100

    feat: change section heading font to semiBold

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,6 +1,6 @@
 export default function SectionHeading({ children }: { children: React.ReactNode }) {
     return (
-        <h2 className="text-3xl font-bold text-slate-800 text-center mb-10 mt-[200px]">
+        <h2 className="text-3xl font-semiBold text-slate-800 text-center mb-10 mt-[200px]">
             {children}
         </h2>
     )
